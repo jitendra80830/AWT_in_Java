e5cc67f6c4e9d1741b9267a4612e2eaa125bbd40
jitendra <jitendrakumar89973@gmail.com>
2021-01-16 15:03:53 +0530

    Created a Scrollbar

--- ScrollbarExample.java
+++ ScrollbarExample.java
@@ -1,1 +1,17 @@
 //Create a Scrollbar
+import java.awt.*;
+
+public class ScrollbarExample{
+	ScrollbarExample(){
+		Frame f=new Frame();
+		Scrollbar s=new Scrollbar();
+		s.setBounds(100,100,50,50);
+		f.add(s);
+		f.setSize(400,400);
+		f.setLayout(null);
+		f.setVisible(true);
+	}
+	public static void main(String[] args){
+		new ScrollbarExample();
+	}
+}
